7357c7ad846b19873a6d68e40a5957d44e5f9e98
hultingk <hultingk@msu.edu>
2024-09-20 14:46:00 -0700

    pollinator and floral species string cleaning

--- 01-data-cleaning.R
+++ 01-data-cleaning.R
@@ -2,3 +2,37 @@
 #### Data cleaning script ####
 # Author: Katherine Hulting, hultingk@msu.edu
 # -------------------------------------- #
+
+# loading libaries
+library(tidyverse)
+
+# loading data 
+pollinator <- read.csv(file = file.path("data", "2024-SRS-plant-pollinator.csv"))
+
+# removing wasps, lost insects, unknowns, no observations
+pollinator <- pollinator %>%
+  filter(!pollinator_common %in% c("Wasp", "unknown butterfly", "unknown skipper", "No associated insect", "0", "WASP", "LOST", "NOT A POLLINATOR"))
+
+
+# --------------------------- #
+#### flower species cleaning ####
+# --------------------------- #
+# combining Eupatorium glaucescens and Eupatorium linearifolium as one species
+pollinator$flower_species <- str_replace(pollinator$flower_species, "Eupatorium glaucescens", "Eupatorium linearifolium")
+
+# checking flower species
+pollinator %>%
+  count(flower_species)
+
+# --------------------------- #
+#### pollinator species cleaning ####
+# --------------------------- #
+# grouping Erynnis species 
+pollinator$pollinator_species <- str_replace(pollinator$pollinator_species, "Erynnis horatius", "Erynnis sp.")
+pollinator$pollinator_species <- str_replace(pollinator$pollinator_species, "Erynnis zarucco", "Erynnis sp.")
+
+# checking pollinator species
+pollinator %>%
+  count(pollinator_species)
+
+
