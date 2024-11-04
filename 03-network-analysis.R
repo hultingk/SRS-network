fd93ee46f160e6e3e1004cab826f05c02dd11e51
hultingk <hultingk@msu.edu>
2024-11-04 11:49:11 -0500

    adding plotting

--- 03-network-analysis.R
+++ 03-network-analysis.R
@@ -23,15 +23,40 @@ pollinator %>%
 #### Interaction abundance ####
 # calculate abundance of interactions per patch and sampling round
 abundance <- pollinator %>%
-  count(block, patch, sampling_round)
+  count(block, patch)
 
 # how does connectivity affect the abundance of plant-pollinator interactions? 
-abund_mod <- glmmTMB(n ~ patch + (1|block) + (1|sampling_round), # block is random effect
+m1 <- glmmTMB(n ~ patch + (1|block), # block is random effect
               data = abundance,
               family = nbinom2) # negative binomial family for overdispersed count data
-summary(abund_mod) # model output
-plot(simulateResiduals(abund_mod)) # looks good
-check_overdispersion(abund_mod) # no overdispersion for nbinom, overdispersed when family = poisson
+summary(m1) # model output
+plot(simulateResiduals(m1)) # looks good
+check_overdispersion(m1) # no overdispersion for nbinom, overdispersed when family = poisson
+
+
+# model predictions for plotting
+m1.df <- abundance %>%
+  dplyr::select(c("block", "patch", "n"))
+m1.df$abund_pred <- exp(predict(m1, re.form = NA))
+m1.df$patch <- factor(m1.df$patch, levels = c("B", "W"))
+# plotting
+interaction.abun.pred <- m1.df %>%
+    ggplot() +
+    geom_point(aes(x = patch, y = n, color = patch), size = 7, alpha = 0.7) + 
+    geom_line(aes(x = patch, y = n, group = block), size = 1.5, color = "black", alpha = 0.2) +
+    geom_line(aes(x = patch, y = abund_pred, group = block), size = 2) +
+    scale_x_discrete(labels = c('Connected', 'Winged')) +
+    scale_color_manual(values=c("#506D8F","#E2A03C")) +
+    xlab("Patch Type") +
+    ylab(expression(paste("Interaction Abundance"))) +
+    theme_classic() +
+    theme(legend.position = "none") +
+    theme(axis.text = element_text(size = 26)) + # axis tick mark size
+    theme(axis.title = element_text(size = 30)) #+ # axis label size
+interaction.abun.pred
+pdf(file = file.path("plots", "interaction-abund.pdf"), width = 10, height = 8)
+interaction.abun.pred
+dev.off()
 
 
 #### Floral diversity ####
@@ -47,27 +72,27 @@ summarize_pollinator <- pollinator %>%
 summarize_pollinator$fl.diversity <- fl.diversity
 
 # how does connectivity affect the diversity of flowers that pollinators forage on?
-fl_div_mod <- glmmTMB(fl.diversity ~ patch + (1|block), # block is random effect
+m2 <- glmmTMB(fl.diversity ~ patch + (1|block), # block is random effect
               data = summarize_pollinator, 
               family = "gaussian") # gaussian family for non-integer continuous data
-summary(fl_div_mod) # model summary
-plot(simulateResiduals(fl_div_mod)) # looks good
+summary(m2) # model summary
+plot(simulateResiduals(m2)) # looks good
 
 
 # plotting richness model predictions
-fl_div_mod.predict <- ggpredict(fl_div_mod, terms=c("patch [all]"), back.transform = T, allow.new.levels=TRUE)
-fl_div_mod.stat.test <- tibble::tribble( # creating tibble of p-values for plotting
+m2.predict <- ggpredict(m2, terms=c("patch [all]"), back.transform = T, allow.new.levels=TRUE)
+m2.stat.test <- tibble::tribble( # creating tibble of p-values for plotting
   ~group1, ~group2,   ~p.adj,
   "B",     "W", "0.01"
 )
-fl_div_mod_plot <- fl_div_mod.predict %>%
+m2_plot <- m2.predict %>%
   ggplot(aes(x = x, y = predicted)) + # plotting predicted values
   geom_jitter(aes(x = patch, y = fl.diversity), data = summarize_pollinator, alpha = 0.2, width = 0.1, height = 0.1, size = 5)+ # adding raw data points
   geom_point(size = 5)+  # size of center prediction point
   geom_errorbar(aes(ymin = conf.low, ymax = conf.high), width = 0.15, linewidth = 2.5) + # adding error bars of predicted confidence intervals
   theme_classic() + # choosing simplest ggplot theme
   stat_pvalue_manual( # adding p-values to plot
-    fl_div_mod.stat.test, 
+    m2.stat.test, 
     size = 9,# size of letters
     bracket.size = 1.5,
     y.position = 3.2, step.increase = 0.12, # position of brakets
@@ -82,8 +107,30 @@ fl_div_mod_plot <- fl_div_mod.predict %>%
 # ylim(2.5, 12) # manually setting y axis limits to include p-value brackets
 fl_div_mod_plot
 
+# alternative plots
+# model predictions for plotting
+m2.df <- summarize_pollinator %>%
+  dplyr::select(c("block", "patch", "fl.diversity"))
+m2.df$div_pred <- predict(m2, re.form = NA)
+m2.df$patch <- factor(m2.df$patch, levels = c("B", "W"))
+# plotting
+fl.div.pred <- m2.df %>%
+    ggplot() +
+    geom_point(aes(x = patch, y = fl.diversity, color = patch), size = 7, alpha = 0.7) + 
+    geom_line(aes(x = patch, y = fl.diversity, group = block), size = 1.5, color = "black", alpha = 0.2) +
+    geom_line(aes(x = patch, y = div_pred, group = block), size = 2) +
+    scale_x_discrete(labels = c('Connected', 'Winged')) +
+    scale_color_manual(values=c("#506D8F","#E2A03C")) +
+    xlab("Patch Type") +
+    ylab(expression(paste("Floral Foraging Diversity"))) +
+    theme_classic() +
+    theme(legend.position = "none") +
+    theme(axis.text = element_text(size = 26)) + # axis tick mark size
+    theme(axis.title = element_text(size = 30)) #+ # axis label size
+fl.div.pred
 
-
-
+pdf(file = file.path("plots", "floral-diversity.pdf"), width = 10, height = 8)
+fl.div.pred
+dev.off()
 
 
