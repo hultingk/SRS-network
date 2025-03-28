d1e94af1e3ddd44dc83ac48ba57cbfbede08fa32
hultingk <hultingk@msu.edu>
2025-03-28 15:21:59 -0400

    adding nmds

--- 03-network-analysis.R
+++ 03-network-analysis.R
@@ -379,3 +379,136 @@ plot(simulateResiduals(m3)) # looks good
 
 
 
+
+
+#### community composition? ####
+community.comp <- pollinator %>%
+  #mutate(unique_ID = paste(block, patch, sep = ".")) %>%
+  #mutate(interaction = paste(pollinator_analysis, flower_species, sep = "-")) %>%
+  #count(unique_ID, interaction) %>%
+  count(unique_ID, pollinator_analysis) %>%
+  pivot_wider(names_from = pollinator_analysis, values_from = n, values_fill = 0)
+community.comp.no.site <- community.comp[-1] # deleting site column
+
+set.seed(300) # setting seed to reproduce later
+# Choosing number of dimensions
+# Specifying the raw data models for all dimensions k
+mods.r <- list() # an empty list to hold model objects
+stress.r <- numeric() # an empty numeric vector to hold final stress values 
+conv.r <- character() # an empty character vector to hold convergence messages 
+for(i in 1:7){
+  mod <- metaMDS(community.comp.no.site, distance = 'bray', k = i, try = 20, trymax = 1000)
+  mods.r[[as.character(i)]] <- mod 
+  stress.r[i] <- mod$stress 
+  conv.r[i] <- mod$converged
+}
+# scree plot
+palette(c('red', 'black'))
+plot(x = 1:7, y = stress.r, main = 'Raw Data', xlab = 'Dimensionality',
+     ylab = 'Stress',
+     pch = 19, col = factor(conv.r))
+# red points are dimensions that there was no stable solution for
+
+
+# Actual NMDS
+comp.mds <- metaMDS(community.comp.no.site, distance = "bray", k = 3, autotransform = F, pc = T, trymax = 1000)
+
+# Calculating how much variance in community data the NMDS accounts for
+bcd <- vegdist(community.comp.no.site, method = 'bray')
+r.ed <- dist(comp.mds$points[, 1:2]) # Calculating distance between sites according to the first two axes
+cor(r.ed, bcd, method = 'spearman') # Calculating Spearman's rho
+
+# Shepard plot, shows fit
+stressplot(comp.mds) # looks good
+
+
+# PERMANOVA patch type - testing if community composition differs between connected and winged patches
+obs <- community.comp.no.site # community data
+spp <- community.comp %>% 
+  dplyr::select(unique_ID) %>%
+  separate(unique_ID, c("block", "patch")) %>%
+  dplyr::select(patch) # separating patch type
+
+d.manova <- adonis2(obs ~ patch, method = "bray", data= spp) # testing difference between patch types
+d.manova # summary
+
+#### DIFFERENCES IN DISPERSION
+# Compute distance to group centroid (dispersion analysis)
+dispersion <- betadisper(vegdist(obs, method = "bray"), spp$patch)
+
+# Perform a permutation test for homogeneity of dispersions
+dispersion_test <- permutest(dispersion, permutations = 999)
+
+# Print test results
+print(dispersion_test)
+
+# Plot the dispersion results
+plot(dispersion, main = "Group Dispersion in NMDS Space")
+
+# Enhanced ggplot visualization of NMDS with group dispersions
+nmds_df <- as.data.frame(comp.mds$points)
+nmds_df$Group <- spp$patch
+
+ggplot(nmds_df, aes(x = MDS1, y = MDS2, color = Group)) +
+  geom_point(size = 3) +
+  stat_ellipse() +
+  theme_minimal() +
+  labs(title = "NMDS Plot with Group Dispersions",
+       x = "NMDS1", y = "NMDS2")
+
+
+
+
+
+
+
+# plotting NMDS
+plot.community <- community.comp %>%
+  separate(unique_ID, c("block", "patch")) %>%
+  mutate(patch.color = if_else(patch == "B", 1, 2)) # adding #s that will correspond to plot colors
+
+palette(c('thistle', 'lightblue')) # setting plot colors
+#plotting
+par(mar = c(5.1, 4.5, 4.1, 2.1))
+p1 <- plot(x = comp.mds$points[, 1], # x axis = 1st dimension
+           y = comp.mds$points[, 2], # y axis = 2nd dimension
+           pch = 19, # point shapes
+           cex = 2, # point size
+           col = plot.community$patch.color, # point colors, correspond to patch type
+           xlab = 'NMDS Axis 1', # x-axis label
+           ylab = 'NMDS Axis 2', # y-axis label
+           cex.axis = 1.5, # axis number size
+           cex.lab = 1.9#, # axis label size
+           #xlim = c(-2.4, 1.7), # plot x-axis size
+           #ylim = c(-1.5, 1.4)
+           ) # plot y-axis size
+# Plotting ellipse polygons 
+ordiellipse(comp.mds, 
+            groups = plot.community$patch, # groups are patch type
+            col = plot.community$patch.color, # ellipse colors, correspond to patch type
+            draw = 'polygon', # type of ellipse
+            kind = 'sd', # ellipse is drawing standard deviation of points
+            alpha = 140, # transparency of ellipse
+            border = plot.community$patch.color)# border color, correspond to patch type
+legend("topleft", # adding legend to top left corner
+       inset=c(0, -0.03), # exact legend locations
+       legend = c('Connected Patch', 'Winged Patch'), # legend names
+       bty = 'n', # no box drawn around legend
+       pch = 19, # legend point shape
+       col = plot.community$patch.color, # legend colors, correspond to patch type
+       cex = 1.8, # legend size
+       y.intersp = 0.4,
+       x.intersp = 0.3) # vertical distance between legend lines
+text(-2.1, -1.5, cex = 1.5, paste("stress =", round(comp.mds$stress, digits = 3))) # adding stress value to plot
+nmds.plot <- recordPlot() # saving plot as an object
+
+
+
+
+
+
+
+
+
+
+
