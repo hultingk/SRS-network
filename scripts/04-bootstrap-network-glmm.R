c614a5d54efbf487c8c81d1b4fc1264991f5dbe3
hultingk <hultingk@msu.edu>
2026-06-12 16:01:53 -0400

    plotting

--- scripts/04-bootstrap-network-glmm.R
+++ scripts/04-bootstrap-network-glmm.R
@@ -6,6 +6,38 @@ network_metrics_boot <- read.csv(file = file.path("data", "L2_boot_metrics", "ne
 m_nodf <- glmmTMB(nodf ~ patch + (1|block),
                   data = network_metrics_boot)
 summary(m_nodf)
+# nestedness plot
+# plotting nodf per species
+m.nodf.df <- ggpredict(m_nodf, terms = c("patch"), back_transform = TRUE)
+# plotting
+nodf.pred <- m.nodf.df %>%
+  ggplot() +
+  geom_jitter(aes(x = patch, y = nodf, color = patch), data = network_metrics_boot, size = 6, alpha = 0.55,
+              width = 0.08, height = 0) +
+  geom_errorbar(aes(x = x, y = predicted, ymin = conf.low, ymax = conf.high, fill = x), color = "black",
+                data = m.nodf.df, width = 0, linewidth = 2.5) +
+  geom_line(aes(x = x, y = predicted, group = group), linewidth = 2, linetype = 2) +
+  geom_point(aes(x = x, y = predicted, fill = x), size = 8, colour="black", pch=21, stroke = 2) +
+  scale_x_discrete(labels = c('Connected', 'Unconnected')) +
+  theme_classic(base_size = 28) +
+  theme(panel.border = element_rect(colour = "black", fill=NA, linewidth=1),
+        # panel.grid.major = element_line(linetype = 2, linewidth = 0.7, color = "grey85"), 
+        panel.grid.minor = element_blank(),
+        axis.ticks = element_line(color = "black", linewidth = 0.7),
+        strip.text.x = element_text(hjust = -0.05),
+        panel.background = element_rect(fill = "transparent", color = NA), # Inside axes
+        plot.background = element_rect(fill = "transparent", color = NA)) +
+  scale_color_manual(values=c("#1E395FFF","#5B859EFF")) +
+  scale_fill_manual(values=c("#1E395FFF","#5B859EFF")) +
+  xlab("Patch type") +
+  ylab(expression("NODF")) +
+  theme(legend.position = "none") 
+nodf.pred
+
+# pdf(file = file.path("plots", "nodf.pred.pdf"), width = 7, height = 6.5)
+# nodf.pred
+# dev.off()
+
 
 # links per species 
 m_links <- glmmTMB(links_per_sp ~ patch + (1|block),
@@ -39,9 +71,9 @@ links.pred <- m.links.df %>%
   theme(legend.position = "none") 
 links.pred
 
-pdf(file = file.path("plots", "links.pred.pdf"), width = 7, height = 6.5)
-links.pred
-dev.off()
+# pdf(file = file.path("plots", "links.pred.pdf"), width = 7, height = 6.5)
+# links.pred
+# dev.off()
 
 
 
