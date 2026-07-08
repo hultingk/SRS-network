9cb856c789e73da457aa62298697c4dcab6b289e
hultingk <hultingk@msu.edu>
2026-07-08 11:54:38 -0400

    trying out individual plant specialization

--- scripts/03c-network-d.R
+++ scripts/03c-network-d.R
@@ -42,3 +42,29 @@ net_d_mean <- higher_level_d %>%
 
 # write.csv(net_d_mean, file = file.path("data", "L2_boot_metrics", "net_d_mean.csv"), row.names = FALSE)
 
+# #### d' for only dominant species ####
+# species_net_d <- lapply(webs.matrix, dfun)
+# species_net_d <- lapply(species_net_d, function(x) x[1])
+# species_net_d.df <- lapply(species_net_d, unlist)
+# species_net_d.df <- purrr::imap_dfr(species_net_d.df, ~{
+#   as.data.frame(as.list(unlist(.x))) %>%
+#     mutate(unique_ID = .y)
+# })
+# 
+# species_net_d.df <- species_net_d.df %>%
+#   dplyr::select(unique_ID, dprime.Rhus.copallinum, dprime.Aureolaria.pectinata, dprime.Pityopsis.sp., 
+#                 dprime.Centrosema.virginianum, dprime.Diodia.teres) %>%
+#   separate(unique_ID, into = c("block", "patch"))
+# 
+# m <- glmmTMB(dprime.Diodia.teres ~ patch + (1|block), 
+#              data = species_net_d.df)
+# summary(m)
+# 
+# 
+# m <- glmmTMB(dprime.Pityopsis.sp. ~ patch + (1|block), 
+#              data = species_net_d.df)
+# summary(m)
+# 
+# pollinator %>%
+#   dplyr::count(flower_species) %>%
+#   View()
